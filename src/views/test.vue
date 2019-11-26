e825f1194219ad6afa6ba9f9f37b36160ba35ef5
雷银-20669 <yin.lei@hand-china.com>
2019-11-26 18:13:06 +0800

    indexedDB快速入门

--- src/views/test.vue
+++ src/views/test.vue
@@ -28,8 +28,106 @@ export default {
     if (!this.indexedDB) {
       console.log('你的浏览器不支持IndexedDB')
     }
+    // 1. 打开数据库 数据库存在 ？onsuccess/onerror ：onupgradeneeded => onsuccess/onerror
+    let db = await this.openDB()
+
+    // 2.新增数据
+    // this.getDataList()
+    // this.dataList.forEach(ele => {
+    //   this.addData(db, 'person', ele)
+    // })
+
+    // 3. 查询数据  主键查询 游标查询 索引查询 游标和索引查询
+    // （1）主键查询
+    this.dataItem = await this.getDataByKey(db, 'person', 9)
+    // (2)游标查询
+    // this.cursorGetData(db, 'person')
+    // (3) 索引查询
+    // this.getDataByIndex(db, 'person', 'address', '云南省')
+    // （4）游标和索引查询
+    // this.cursorGetDataByIndex(db, 'person', 'address', '云南省')
+
+    // 4.更新数据
+    // this.dataItem.flag = 9
+    // this.updateData(db, 'person', this.dataItem)
+
+    // 5.删除数据
+    // (1) 主键
+    // this.deleteData(db, 'person', 8)
+    // （2）游标删除
+    // this.cursorDeleteData(db, 'person', 'address', '云南省')
+
+    // 6.关闭和删除数据库
+    this.closeDB(db)
+    this.deletedb()
   },
   methods: {
+    // 7.更新数据 db：数据库实例 tableName：表名称 data：数据
+    updateData: function (db, storename, data) {
+      var request = db.transaction([storename], 'readwrite') // 事务对象
+        .objectStore(storename) // 仓库对象
+        .put(data)
+
+      request.onsuccess = function () {
+        console.log('数据更新成功')
+      }
+
+      request.onerror = function () {
+        console.log('数据更新失败')
+      }
+    },
+    // 8.删除数据 db：数据库实例 tableName：表名称 id：主键值
+    deleteData: function (db, storename, id) {
+      var request = db.transaction([storename], 'readwrite') // 事务
+        .objectStore(storename) // 仓库对象
+        .delete(id)
+
+      request.onsuccess = function () {
+        console.log('数据删除成功')
+      }
+
+      request.onerror = function () {
+        console.log('数据删除失败')
+      }
+    },
+    // 9.通过索引和游标删除指定的数据 db:数据库实例  storename:表名称   indexName：索引名  indexValue：索引值
+    cursorDeleteData: function (db, storename, indexName, indexValue) {
+      var store = db.transaction(storename, 'readwrite').objectStore(storename) // 仓库对象
+      var request = store.index(indexName) // 索引对象
+        .openCursor(IDBKeyRange.only(indexValue)) // 指针对象
+      request.onsuccess = function (e) {
+        var cursor = e.target.result
+        var deleteRequest
+        if (cursor) {
+          deleteRequest = cursor.delete()// 请求删除当前项
+          deleteRequest.onerror = function () {
+            console.log('游标删除该记录失败')
+          }
+          deleteRequest.onsuccess = function () {
+            console.log('游标删除该记录成功')
+          }
+          cursor.continue()
+        }
+      }
+      request.onerror = function (e) {
+      }
+    },
+    // 10.关闭数据库 db：数据库实例
+    closeDB: function (db) {
+      db.close()
+      console.log('数据库已关闭')
+    },
+    // 11.删除数据库 dbname：数据库名称
+    deletedb: function () {
+      var self = this
+      let deleteRequest = self.indexedDB.deleteDatabase('databaseName')
+      deleteRequest.onerror = function (event) {
+        console.log('删除失败')
+      }
+      deleteRequest.onsuccess = function (event) {
+        console.log('删除成功')
+      }
+    },
     // 1.打开数据库
     openDB () {
       return new Promise((resolve, reject) => {
@@ -156,72 +254,6 @@ export default {
         }
       }
       request.onerror = function (e) {
-      }
-    },
-    // 7.更新数据 db：数据库实例 tableName：表名称 data：数据
-    updateData: function (db, storename, data) {
-      var request = db.transaction([storename], 'readwrite') // 事务对象
-        .objectStore(storename) // 仓库对象
-        .put(data)
-
-      request.onsuccess = function () {
-        console.log('数据更新成功')
-      }
-
-      request.onerror = function () {
-        console.log('数据更新失败')
-      }
-    },
-    // 8.删除数据 db：数据库实例 tableName：表名称 id：主键值
-    deleteData: function (db, storename, id) {
-      var request = db.transaction([storename], 'readwrite') // 事务
-        .objectStore(storename) // 仓库对象
-        .delete(id)
-
-      request.onsuccess = function () {
-        console.log('数据删除成功')
-      }
-
-      request.onerror = function () {
-        console.log('数据删除失败')
-      }
-    },
-    // 9.通过索引和游标删除指定的数据 db:数据库实例  storename:表名称   indexName：索引名  indexValue：索引值
-    cursorDeleteData: function (db, storename, indexName, indexValue) {
-      var store = db.transaction(storename, 'readwrite').objectStore(storename) // 仓库对象
-      var request = store.index(indexName) // 索引对象
-        .openCursor(IDBKeyRange.only(indexValue)) // 指针对象
-      request.onsuccess = function (e) {
-        var cursor = e.target.result
-        var deleteRequest
-        if (cursor) {
-          deleteRequest = cursor.delete()// 请求删除当前项
-          deleteRequest.onerror = function () {
-            console.log('游标删除该记录失败')
-          }
-          deleteRequest.onsuccess = function () {
-            console.log('游标删除该记录成功')
-          }
-          cursor.continue()
-        }
-      }
-      request.onerror = function (e) {
-      }
-    },
-    // 10.关闭数据库 db：数据库实例
-    closeDB: function (db) {
-      db.close()
-      console.log('数据库已关闭')
-    },
-    // 11.删除数据库 dbname：数据库名称
-    deletedb: function () {
-      var self = this
-      let deleteRequest = self.indexedDB.deleteDatabase('databaseName')
-      deleteRequest.onerror = function (event) {
-        console.log('删除失败')
-      }
-      deleteRequest.onsuccess = function (event) {
-        console.log('删除成功')
       }
     }
   }
